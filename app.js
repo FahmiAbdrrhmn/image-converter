b09f7fb60febb9d51fae5876bb537808983eed2c
FahmiAbdrrhmn <fahmiabdurrahman0104@email.com>
2026-06-07 23:27:21 +0700

    fix: vercel ngentot

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const apiRoutes = require('../routes/apiRoutes');
+const apiRoutes = require('./routes/apiRoutes');
 
 const app = express();
 
